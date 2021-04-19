cd52d2e6854f5b0fa6293ebf04eaea7b3b509072
seongeun82 <67671890+seongeun82@users.noreply.github.com>
2021-04-19 20:31:57 +0900

    shopping game

--- chapter3/imgs/coordinates/main.js
+++ chapter3/imgs/coordinates/main.js
@@ -3,16 +3,31 @@ const xline = document.querySelector(".horizontal");
 const yline = document.querySelector(".vertical");
 const tag = document.querySelector(".tag");
 
-document.addEventListener("mousemove", (event) => {
-  x = event.clientX;
-  y = event.clientY;
+addEventListener("load", () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfW = targetRect.width / 2;
+  const targetHalfH = targetRect.height / 2;
 
-  target.style.top = `${y}px`;
-  target.style.left = `${x}px`;
-  tag.style.top = `${y}px`;
-  tag.style.left = `${x}px`;
-  xline.style.top = `${y}px`;
-  yline.style.left = `${x}px`;
+  document.addEventListener("mousemove", (event) => {
+    x = event.clientX;
+    y = event.clientY;
+    // 요소를 움직일 때는 translate 을 사용해서 쓴다.
 
-  tag.innerHTML = `(${x}px, ${y}px)`;
+    // target.style.top = `${y}px`;
+    // target.style.left = `${x}px`;
+
+    // tag.style.top = `${y}px`;
+    // tag.style.left = `${x}px`;
+    // xline.style.top = `${y}px`;
+    // yline.style.left = `${x}px`;
+
+    tag.style.transform = `translate(${x}px, ${y}px)`;
+    target.style.transform = `translate(${x - targetHalfW}px,  ${
+      y - targetHalfH
+    }px)`;
+    xline.style.transform = `translateY(${y}px)`;
+    yline.style.transform = `translateX(${x}px)`;
+
+    tag.innerHTML = `(${x}px, ${y}px)`;
+  });
 });

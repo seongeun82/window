59cf9fefa5b1784d9a068d92ee444e31ed0a7d17
seongeun82 <67671890+seongeun82@users.noreply.github.com>
2021-04-29 21:35:09 +0900

    impoved shopping List App

--- main.js
+++ main.js
@@ -22,31 +22,21 @@ function onAdd() {
   input.focus();
 }
 
+let id = 0; //uuid
 function createItem(text) {
   const itemRow = document.createElement("li");
   itemRow.setAttribute("class", "item__row");
-
-  const item = document.createElement("div");
-  item.setAttribute("class", "item");
-
-  const name = document.createElement("span");
-  name.setAttribute("class", "item__name");
-  name.innerText = text;
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.setAttribute("class", "item__delete");
-  deleteBtn.innerHTML = '<i class= "fas fa-trash-alt"></i>';
-  // deleteBtn.addEventListener("click", () => {
-  //   items.removeChild(itemRow);
-  // });
-
-  const itemDivider = document.createElement("div");
-  itemDivider.setAttribute("class", "item__divider");
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
-
+  itemRow.setAttribute("data-id", id);
+  itemRow.innerHTML = `
+  <div class="item">
+    <span class="item__name">${text}</span>
+    <button class="item__delete">
+      <i class="fas fa-trash-alt" data-id = ${id}></i>
+    </button>
+  </div>
+  <div class="item__divider"></div>
+</li>`;
+  id++;
   return itemRow;
 }
 
@@ -63,7 +53,9 @@ input.addEventListener("keypress", (event) => {
 // event 위임을 활용해서 클릭되었을 때 아이템 삭제하기
 
 items.addEventListener("click", (event) => {
-  // 문제점 고유한 id 를 찾아서 하는 법 : global id uuid 를 활용할 수 있다.
-  // 현재의 상태는 아이템 간의 구분이 없는 상태가 맞음 오호!! 알 것이라고 생각했는데 모르네..
-  console.log(event);
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id='${id}']`);
+    toBeDeleted.remove();
+  }
 });
